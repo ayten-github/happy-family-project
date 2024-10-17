2a23fc55a38f7b04d69843f5d5185c8d2d88c66b
Ayten <ababakishiyeva@std.beu.edu.az>
2024-10-17 22:00:42 +0300

    hw-7 added just human class changed

--- src/main/java/az/edu/strangers/Human.java
+++ src/main/java/az/edu/strangers/Human.java
@@ -4,7 +4,10 @@ import az.edu.strangers.dao.Family;
 
 import java.time.LocalDate;
 import java.time.Period;
+import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
 import java.util.Map;
 import java.util.Objects;
 
@@ -12,7 +15,7 @@ public class Human {
 
     private String name;
     private String surname;
-    private LocalDate birthDate;
+    private long birthDate;
     private Integer IQ;
     private Map<DayOfWeek, String> schedule;
     private Family family;
@@ -21,19 +24,13 @@ public class Human {
     public Human() {
     }
 
-    public Human(String name, String surname, LocalDate birthDate) {
+    public Human(String name, String surname, long birthDate) {
         this.name = name;
         this.surname = surname;
         this.birthDate = birthDate;
     }
-    public Human(String name, String surname, String birthDateStringForAoptedChild, Integer IQ) {
-        this.name = name;
-        this.surname = surname;
-        this.IQ = IQ;
-        this.birthDate = LocalDate.parse(birthDateStringForAoptedChild, DateTimeFormatter.ofPattern("dd/MM/yyyy"));
-    }
 
-    public Human(String name, String surname, LocalDate birthDate, Integer IQ, Map<DayOfWeek, String> schedule, Family family) {
+    public Human(String name, String surname, long birthDate, Integer IQ, Map<DayOfWeek, String> schedule, Family family) {
         this.name = name;
         this.surname = surname;
         this.birthDate = birthDate;
@@ -45,7 +42,7 @@ public class Human {
     public Human(String name, String surname, LocalDate birthday, Integer IQ) {
         this.name = name;
         this.surname = surname;
-        this.birthDate = birthday;
+        this.birthDate = convertLocalDateToMillis(birthday);
         this.IQ = IQ;
     }
 
@@ -65,11 +62,11 @@ public class Human {
         this.surname = surname;
     }
 
-    public LocalDate getYear() {
+    public long getYear() {
         return birthDate;
     }
 
-    public void setYear(LocalDate birthdate) {
+    public void setYear(long birthdate) {
         this.birthDate = birthdate;
     }
 
@@ -97,6 +94,10 @@ public class Human {
         this.family = family;
     }
 
+    public long convertLocalDateToMillis(LocalDate localDate) {
+        return localDate.atStartOfDay(ZoneId.systemDefault()).toInstant().toEpochMilli();
+    }
+
     public void greetPet() {
         if (family != null && family.getPets() != null && !family.getPets().isEmpty()) {
             for (Pet pet : family.getPets()) {
@@ -108,8 +109,18 @@ public class Human {
     }
     public String describeAge() {
         LocalDate currentDate = LocalDate.now();
-        Period age = Period.between(birthDate, currentDate);
-        return String.format("Age: %d years, %d months, and %d days.", age.getYears(), age.getMonths(), age.getDays());
+        LocalDate birthDateInLocal= convertMillisDate(birthDate);
+        long year= ChronoUnit.YEARS.between(birthDateInLocal,currentDate);
+        long month=ChronoUnit.MONTHS.between(birthDateInLocal.plusYears(year),currentDate);
+        long day=ChronoUnit.DAYS.between(birthDateInLocal.plusYears(year).plusMonths(month),currentDate);
+        return String.format("%s was born %d year, %d month, %d day", name, year, month, day);
+
+    }
+
+    public LocalDate convertMillisDate(long milliInDate){
+        return new Date(milliInDate).toInstant().
+                atZone(ZoneId.systemDefault()).
+                toLocalDate();
     }
 
     public void describePet() {
@@ -130,7 +141,7 @@ public class Human {
 
     public String toString() {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-        String formattedBirthDate = birthDate.format(formatter);
+        String formattedBirthDate = convertMillisDate(birthDate).format(formatter);
         return "Human{name='%s', surname='%s', birthdate=%s}".formatted(name, surname, formattedBirthDate);
     }
 
